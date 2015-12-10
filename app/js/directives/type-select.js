27226fd729ede4e028c0ed95e6e5a93509789b89
peter <peter.k.nets@gmail.com>
2015-12-10 13:00:35 +0200

    Add temporary type-select directived; add 'Contact' page

---
+++ app/js/directives/type-select.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Peter on 10.12.2015.
+ */
+'use strict';
+
+angular.module('bobbleApp')
+  .directive('typeSelect', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/directives/type-select.html'
+    }
+  });
